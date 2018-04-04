9a1b6e5ee4d7bc128110680cb1262746fbc4c275
Craig <craig@cra.io>
2018-04-04 23:42:05 +0100

    Noo

--- assets/script/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    'use strict';
-    var demangle = baffle(".demangle");
-    demangle.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█+-#@0',
-    });
-    demangle.reveal(6283185307179586476925286766559);
-});
